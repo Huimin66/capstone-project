1baa565f0e6d45be8527791222ffe6b65fdeb3b2
Huimin Cao <huim.cao@gmail.com>
2022-06-28 14:30:37 +0200

    Add show menus test

--- src/ShowMenus/ShowMenus.spec.js
+++ src/ShowMenus/ShowMenus.spec.js
@@ -1,8 +1,7 @@
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
-import ShowMenus from './ShowMenus.js';
 import menusdata from '../data.js';
+import ShowMenus from './ShowMenus.js';
 
 describe('ShowMenus', () => {
   it('find all menus in the page', () => {
