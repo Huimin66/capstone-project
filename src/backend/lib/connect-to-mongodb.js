18c547dc34e3a951136bfa7d435a4efdf10ae3ce
Huimin Cao <huim.cao@gmail.com>
2022-07-22 20:36:49 +0200

    Fix lint

--- src/backend/lib/connect-to-mongodb.js
+++ src/backend/lib/connect-to-mongodb.js
@@ -6,11 +6,6 @@ if (!MONGODB_URI) {
   throw new Error('MONGODB_URI is undefined');
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
 let cached = global.mongoose;
 
 if (!cached) {
